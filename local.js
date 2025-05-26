51910bced69132e853e296c4a664a893d67dd1ed
Harsh Kaushik <kaushikharsh1947@gmail.com>
2025-05-26 11:15:11 +0530

    Some other changes in files

---
+++ local.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const PORT = 2000;
+const hostname = "localhost";
+const home = fs.readFileSync("./index.html", "utf-8");
+
+console.log(__filename);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    return res.end(home);
+  }
+  if (req.url === "/about") {
+    return res.end("<h1> ABOUT PAGE </h1>");
+  }
+  if (req.url === "/contact") {
+    return res.end("<h1>CONTACT PAGE </h1>");
+  }
+  if (req.url === "/service") {
+    return res.end("<h1>SERVICE PAGE</h1>");
+  } else {
+    return res.end("<h1> 404 Page Not Found </h1>");
+  }
+});
+
+server.listen(PORT, hostname, () => {
+  console.log(`Server is working on http://${hostname}:${PORT}`);
+});
